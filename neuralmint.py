49ebba33d52835fe5aaa0189623fb4770c41d4a8
Devnilethegreat <212276580+Devnilethegreat@users.noreply.github.com>
2025-03-24 21:34:22 +0000

    Scaffold core module structure

---
+++ neuralmint.py
@@ -0,0 +1,16 @@
+# neuralmint.py
+"""
+Main module for NeuralMint application.
+"""
+
+import argparse
+import logging
+import os
+import sys
+from typing import Optional
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class NeuralMintCore:
